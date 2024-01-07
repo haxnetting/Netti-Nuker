4259761794b3c120fa4e7dc6e9c192540efa1fa1
The Netting Guy <87181561+haxnetting@users.noreply.github.com>
2024-01-07 13:28:45 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+nicknames = "Hax Netting NUKER"
+ban_reason = "HAXNET"
+nuke_message = """
+> :bomb:  NUKED BY YOU
+> :link: YOU
+> :heavy_plus_sign:  @everyone @here
+"""
+role_rename = "Hax Netting"
---
+++ pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.4.6 (trial), 000000, 2024-01-07T13:23:34.911183
+from .pyarmor_runtime import __pyarmor__
